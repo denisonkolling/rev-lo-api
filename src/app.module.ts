c1c205ac3c49676faeac4138a9d1efe4118be8d9
Denison Kolling <denisonkolling@gmail.com>
2024-09-28 08:48:10 -0300

    feat(reviews): add reviews resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ReviewModule } from './review/review.module';
+import { ReviewsModule } from './reviews/reviews.module';
+
 
 @Module({
-  imports: [ReviewModule],
+  imports: [ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
