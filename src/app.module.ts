4258c4bb8b27ccc39ecf1a736c41cfd1439e1dbe
Denison Kolling <denisonkolling@gmail.com>
2024-09-28 08:56:29 -0300

    chore: add nest config and env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReviewsModule } from './reviews/reviews.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 
 @Module({
-  imports: [ReviewsModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    load: [configuration],
+    envFilePath: [`.env.${process.env.NODE_ENV}`, '.env'],
+  }), ReviewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,6 @@
+export default () => ({
+    google: {
+        placesApiKey: process.env.GOOGLE_PLACES_API_KEY,
+        placesBaseUrl: process.env.GOOGLE_PLACES_BASE_URL
+    }
+});
